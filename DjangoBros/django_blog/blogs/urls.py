e09fdab17093b09176830eaeef8e87390f9f4ae6
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-07-18 14:23:27 +0900

    DjangoMemo

--- DjangoBros/django_blog/blogs/urls.py
+++ DjangoBros/django_blog/blogs/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'blogs'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/<int:blog_id>/', views.detail, name='detail'),
 ]
--- DjangoBros/django_blog/blogs/views.py
+++ DjangoBros/django_blog/blogs/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Blog
 
 def index(request):
-    return render(request, 'blogs/index.html')
+    blogs = Blog.objects.order_by('-created_datetime')
+    return render(request, 'blogs/index.html', {'blogs': blogs})
 
-# Create your views here.
+def detail(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    return render(request, 'blogs/detail.html', {'blog': blog})
