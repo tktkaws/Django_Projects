f99f957aa65d7c3486e43808066a510a6d007b1e
takayuki-takahashi-dic <ttkaws15@gmail.com>
2020-07-18 14:47:08 +0900

    インスタンスを表示する https://djangobrothers.com/tutorials/memo_app/display_memos/

--- DjangoMemo/memo/app/urls.py
+++ DjangoMemo/memo/app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'app'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:memo_id>', views.detail, name='detail'),
+    path('new_memo', views.new_memo, name='new_memo'),
 ]
--- DjangoMemo/memo/app/views.py
+++ DjangoMemo/memo/app/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Memo
+from django.shortcuts import get_object_or_404
+
 
 def index(request):
-    return render(request, 'app/index.html')
+    memos = Memo.objects.all().order_by('-updated_datetime')
+    return render(request, 'app/index.html', {'memos': memos})
+
+
+def detail(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    return render(request, 'app/detail.html', {'memo': memo})
+
+
+def new_memo(request):
+    return render(request, 'app/new_memo.html')
